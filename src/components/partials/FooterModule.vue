f04ced20e00aca0ca955de9ce6fc1f5186b3300d
Martin Erpicum <martin@erpicum.net>
2026-03-09 13:30:43 +0100

    Update footer source code link to point to the correct repository

--- src/components/partials/FooterModule.vue
+++ src/components/partials/FooterModule.vue
@@ -4,7 +4,7 @@
       <div class="mt-8 md:order-1 md:mt-0">
         <p class="text-left text-xs leading-5 text-gray-500">
           Réalisé sous licence libre. <br>
-          <a href="https://github.com/data-cfwb/marches-adeps">Code source</a><br>
+          <a href="https://github.com/tintamarre/marches-adeps">Code source</a><br>
           <span class="italic">Initiative personnelle de Martin Erpicum — Projet non officiel</span>
         </p>
       </div>
